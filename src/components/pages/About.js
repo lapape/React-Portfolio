5aae63a7979f14f184cb177e7d8d24d1d3043554
lapape <lapape07@gmail.com>
2021-06-23 22:09:32 -0600

    added navbar

---
+++ src/components/pages/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <img
+        src="../assets/images/Profilepic.jpg"
+        alt="Photo of Lacey Pape"
+      ></img>
+      <p>
+        My name is Lacey Pape and I am a current full stack web development
+        bootcamp student at the Denver University. Before the bootcamp, I had no
+        experience with coding but was always interested in computers and how
+        they work. I have thoroughly enjoyed learning how to code, and I am very
+        excited to join the industry and broaden my knowledge base even further!
+        The bootcamp has covered many libraries and languages inclduing: HTML,
+        CSS, Javascript, Nodejs, SQL, MongoDB, express, and react.
+      </p>
+    </div>
+  );
+}
